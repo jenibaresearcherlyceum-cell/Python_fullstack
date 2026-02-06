ff401f124e0d1da7354f6846024b3a7b01a643b9
jenibaresearcherlyceum-cell <jenibaresearcherlyceum@gmail.com>
2026-02-06 13:57:19 +0530

    Day 06: Added JSON data persistence and class refinement
    - Implemented to_dict() and from_dict() methods
    - Added JSON file handling for employees and tasks
    - Tested data save and load functionality

---
+++ day06/baseclass.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Student(Person):
+    def __init__(self, name, age, roll_no):
+        super().__init__(name, age)   # parent constructor call
+        self.roll_no = roll_no
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Roll No:", self.roll_no)
+
+
+s1 = Student("Jeni", 20, 101)
+s1.display()
